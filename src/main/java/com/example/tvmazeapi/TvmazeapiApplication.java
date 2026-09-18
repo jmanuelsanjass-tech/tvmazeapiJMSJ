45333ed0eefb0c49b202abbd0390be47f4493a84
P3p3JMSJ <jmanuel.san.jass@gmail.com>
2026-09-18 10:23:22 -0600

    Ambiente

---
+++ src/main/java/com/example/tvmazeapi/TvmazeapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tvmazeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvmazeapiApplication.class, args);
+    }
+
+}
